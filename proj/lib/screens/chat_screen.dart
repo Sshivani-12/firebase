bf33d2423cbb7bce96b2e2e19cf2d14b88596789
Sshivani-12 <shivaniwakde1211@gmail.com>
2020-07-14 21:13:06 +0530

    basic done

---
+++ proj/lib/screens/chat_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx,index)=>Container(
+          padding: EdgeInsets.all(8),
+          child: Text('This is working!!'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          Firestore.instance.collection('chats/hi/messages')
+          .snapshots().listen((data) {
+            print(data.documents[3]['text']);
+           });
+
+
+          
+        },
+        ),
+      
+    );
+  }
+}
