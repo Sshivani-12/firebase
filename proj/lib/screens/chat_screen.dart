3fd1543eb304590446b4b8e5fbdd2c7c0f7328e3
Sshivani-12 <shivaniwakde1211@gmail.com>
2020-07-14 23:21:09 +0530

    added by button  done

--- proj/lib/screens/chat_screen.dart
+++ proj/lib/screens/chat_screen.dart
@@ -4,23 +4,31 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
+      body:StreamBuilder(
+        stream:Firestore.instance.collection('chats/hi/messages')
+          .snapshots() ,
+        builder: (ctx,streamSnapshot){
+     /*     if(streamSnapshot.connectionState==ConnectionState.waiting){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }*/
+        final documents=streamSnapshot.data.documents;    
+        return ListView.builder(
+        itemCount:documents.length,
         itemBuilder: (ctx,index)=>Container(
           padding: EdgeInsets.all(8),
-          child: Text('This is working!!'),
+          child: Text(documents[index]['text']),
         ),
-      ),
+          );
+        },
+      ),  
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: (){
-          Firestore.instance.collection('chats/hi/messages')
-          .snapshots().listen((data) {
-            print(data.documents[3]['text']);
-           });
-
-
-          
+          Firestore.instance
+          .collection('chats/hi/messages')
+          .add({'text':'this is newwwwwbbooorrnn'});
         },
         ),
       
